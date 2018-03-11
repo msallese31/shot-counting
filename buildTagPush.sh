daef17536568f52c4e11172a7d1ae3b565ea9e4c
Mike Sallese <msallese31@gmail.com>
2018-03-11 00:43:38 -0500

    drastically reduce build time

--- buildTagPush.sh
+++ buildTagPush.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+
+set -euo pipefail
+
 # TODO: Add flag to login
 # docker login --username=shotcounterapp --email=shotcounterapp@gmail.com
+
+wheel='false'
+
+while getopts 'w' flag; do
+  case "${flag}" in
+    w) wheel='true' ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+if [ "$wheel" = true ]; then
+    docker build --no-cache -t "revamp_wheel" -f Dockerfile.build .
+fi
 
 docker build -t "shot-counter-backend" .
 
